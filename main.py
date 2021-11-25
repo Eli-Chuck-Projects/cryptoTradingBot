3716ab504818d20a8144ac17da737cdc1bed29a5
Eman12345 <eligfinkel@gmail.com>
2021-11-25 13:53:53 -0600

    stated adding api key

--- main.py
+++ main.py
@@ -1,22 +1,3 @@
-# This is a sample Python script.
+import coinbase
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-def test(num):
-    for i in range(1, 10):
-        print("Hello dickhead")
-        print(i)
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('ajkshdljkadjkfaskenfjkjasef')
-    test(9182490)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+coinbase = coinbase.Coinbase.with_api_key("9qq6rg1tejo", "eYfwHk3x0bM3GeZlHa7+gciwhtKwMu+LFeu2yxQkxAL0Y4yN7Ls0OmAYKcuWz+hmxTLYyUBpKLORdioPZBID2A==")
