373757dc122711956e7720a3c724c9fce22fb6f7
CharlesWeiss-Code <charles.weiss@penncharter.com>
2021-11-27 00:45:25 -0500

    Added comments to all functions in CBFunctions

--- CBFunctions.py
+++ CBFunctions.py
@@ -4,6 +4,7 @@ class CBFunctions:
         self.authClient = authClient
 
     def sell(self, size, market, productID):
+        #Tries to sell and if it cant it returns an error.
         try:
             print(self.authClient.sell(size=size, order_type= market, product_id = productID))
         except:
@@ -11,19 +12,22 @@ class CBFunctions:
 
 
     def getPrice(self, productID):
+        #Returns price and if there is an exception it prints it.
         try:
             price = float(self.authClient.get_product_ticker(product_id=productID)["price"])
             return price
         except:
-            print(float(self.authClient.get_product_ticker(product_id=productID)["price"]))
+            print(self.authClient.get_product_ticker(product_id=productID)["price"])
             return;
 
 
     def printPrice(self, productID):
+        #prints price
         price = float(self.authClient.get_product_ticker(product_id=productID)["price"])
         print("ID: "+str(productID)+" PRICE: $"+str(price)+" TIME: "+str(self.authClient.get_time()))
 
     def buy(self, size, market, productID):
+        #Tries to buy and if it cant it returns the error.
         try:
             print(self.authClient.buy(size=size, order_type = market, product_id= productID))
         except:
