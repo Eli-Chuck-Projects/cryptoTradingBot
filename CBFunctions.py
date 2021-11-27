a82c5660c40a4edd990b710dc0e563df97636a9b
CharlesWeiss-Code <charles.weiss@penncharter.com>
2021-11-27 00:18:19 -0500

    Added get buy function to CBFuntions.

--- CBFunctions.py
+++ CBFunctions.py
@@ -10,3 +10,7 @@ class CBFunctions:
     def getPrice(self, productID):
         self.price = float(self.authClient.get_product_ticker(product_id='productID')['price'])
         print("ID: "+ self.productID+" PRICE: "+ self.price+" TIME: " + self.authClient.get_time())
+
+    def buy(self, size, market, productID):
+        self.authClient.buy(size=self.size, order_type = self.market, product_id= self.productID)
+        print("Bought " + self.productID + " with size of " + self.size + "order type: " + self.market + " at " + self.authClient.get_time())
