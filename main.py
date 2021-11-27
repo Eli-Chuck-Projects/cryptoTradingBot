ee5b1bd49ada651738ec86d3a70bdf71a629b3e0
Eman12345 <eligfinkel@gmail.com>
2021-11-26 22:21:26 -0600

    Added authenticated client

--- main.py
+++ main.py
@@ -3,4 +3,9 @@ import cbpro
 data = open('passphrase', 'r').read().splitlines()
 
 public = data[0]
-passphrase
+passphrase = data[1]
+secret = data[2]
+
+autho_client = cbpro.AuthenticatedClient(public, secret, passphrase)
+
+print(autho_client)
