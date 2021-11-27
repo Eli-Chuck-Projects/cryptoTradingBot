0be17a8b363ac6843d61e8354fdf54cc6e6b6fe0
Eman12345 <eligfinkel@gmail.com>
2021-11-26 22:18:49 -0600

    Added pasphrase

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-from coinbase.wallet.client import Client
-import json
-client = Client("d6a0fb795e982b6a6b134280e4ba96ae", "eYfwHk3x0bM3GeZlHa7+gciwhtKwMu+LFeu2yxQkxAL0Y4yN7Ls0OmAYKcuWz+hmxTLYyUBpKLORdioPZBID2A==")
+import cbpro
 
-user = client.get_current_user();
-print(user)
