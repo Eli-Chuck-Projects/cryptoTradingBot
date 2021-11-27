e012abf5d17483a20d8be84581417a60d75faf91
Eman12345 <eligfinkel@gmail.com>
2021-11-26 23:15:23 -0600

    Merged cbfunctions

--- CBFunctions.py
+++ CBFunctions.py
@@ -3,9 +3,10 @@ class CBFunctions:
     def __init__(self, authClient):
         self.authClient = authClient
 
-    def getUser(self):
-        print(self.authClient.get_accounts())
+    def sell(self, size, market, productId):
+        self.authClient.sell(size=self.size, order_type= self.market, product_id = self.productId)
+        print("Sold " + self.productId + " with size of " + self.size + " order type: " + self.market + "at " + self.authClient.get_time())
 
     def getPrice(self, productID):
-        self.price = self.authClient.get_product_ticker(product_id='productID')['price']
-        print("ID: "+self.productID+" PRICE: "+self.price+" TIME: "+self.authClient.get_time())
+        self.price = float(self.authClient.get_product_ticker(product_id='productID')['price'])
+        print("ID: "+ self.productID+" PRICE: "+ self.price+" TIME: " + self.authClient.get_time())
--- main.py
+++ main.py
@@ -8,38 +8,33 @@ public = data[0]
 passphrase = data[1]
 secret = data[2]
 
-
 print(public)
 print(passphrase)
 print(secret)
 
 auth_client = cbpro.AuthenticatedClient(public, secret, passphrase)
 
+print(auth_client.buy(price="10.0", size="2.1", order_type="limit", product_id="ETH-USD"))
 
-#
 
+# Trading Bot Example
 
-# print(auth_client.buy(price="10.0", size="2.1", order_type="limit", product_id="ETH-USD"))
-#
-#
-# # Trading Bot Example
-#
-# sell_price = 30000
-# sell_amount = 0.3
-#
-# buy_price = 25000
-# buy_amount = 0.2
-#
-# while True:
-#     price = float(auth_client.get_product_ticker(product_id="BTC-USD")["price"])
-#     if price <= buy_price:
-#         print("Buying BTC")
-#         auth_client.buy(size=buy_amount, order_type="marker", product_id="BTC_USD")
-#     elif price >= sell_price:
-#         print("Selling BTC")
-#         auth_client.sell(size=sell_amount, order_type="market", product_id="BTC-USD")
-#     else:
-#         print("Nothing")
-#     time.sleep(10)
+sell_price = 30000
+sell_amount = 0.3
+
+buy_price = 25000
+buy_amount = 0.2
+
+while True:
+    price = float(auth_client.get_product_ticker(product_id="BTC-USD")["price"])
+    if price <= buy_price:
+        print("Buying BTC")
+        auth_client.buy(size=buy_amount, order_type="marker", product_id="BTC_USD")
+    elif price >= sell_price:
+        print("Selling BTC")
+        auth_client.sell(size=sell_amount, order_type="market", product_id="BTC-USD")
+    else:
+        print("Nothing")
+    time.sleep(10)
 
 
